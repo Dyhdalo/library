f0f5dd1bf2a23097e863e9ace1c48a0e3b0902ca
Dima Zvashiy <bengorde80@gmail.com>
2016-02-23 00:02:52 +0200

    Merge branch 'master' of https://github.com/NaukmaLibrary/library
    Conflicts:
    	pom.xml

--- src/main/java/ukma/library/server/dao/BookDao.java
+++ src/main/java/ukma/library/server/dao/BookDao.java
@@ -2,11 +2,12 @@ package ukma.library.server.dao;
 
 import ukma.library.server.entity.Book;
 
+import java.util.Date;
 import java.util.List;
 
 public interface BookDao {
 
-	public boolean addBook(String title, String author, int year, String edition);
+	public boolean addBook(String title, String author, Date date, String edition);
 
 	public Book getBook(int id);
 
--- src/main/java/ukma/library/server/dao/impl/JdbcBookDao.java
+++ src/main/java/ukma/library/server/dao/impl/JdbcBookDao.java
@@ -1,25 +1,61 @@
 package ukma.library.server.dao.impl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
+import java.sql.Date;
+import java.util.*;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import ukma.library.server.dao.BookDao;
 import ukma.library.server.entity.Book;
-import ukma.library.server.entity.User;
 import ukma.library.server.service.LibraryService;
 
 public class JdbcBookDao implements BookDao {
 
+	private final String GET_ALL_BOOKS = "SELECT * from acsm_b775c39c99325aa.Book";
 
-	public boolean addBook(String title, String author, int year, String edition) {
-		return false;
+	private final String ADD_NEW_BOOK = "INSERT INTO acsm_b775c39c99325aa.Book (author, title, edition ,year) VALUES (?,?,?,?)";
+
+
+	private static Connection createConnection(){
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(LibraryService.MYSQL_URL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	private static void closeConnection(Connection connection){
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public boolean addBook(String title, String author, java.util.Date date, String edition) {
+		boolean flag = true;
+
+		Connection conn = createConnection();
+		PreparedStatement statement = null;
+
+		try {
+			statement = conn.prepareStatement(ADD_NEW_BOOK);
+			statement.setString(1,author);
+			statement.setString(2,title);
+			statement.setString(3,edition);
+			statement.setDate(4,new Date(date.getTime()));
+		} catch (SQLException e) {
+			flag = false;
+			e.printStackTrace();
+		}
+
+		closeConnection(conn);
+			return flag;
 	}
 
 	public Book getBook(int id) {
@@ -33,33 +69,37 @@ public class JdbcBookDao implements BookDao {
 	public List<Book> getAllBooks() {
 		
 		ArrayList<Book> books = new ArrayList<Book>();
-		
-		try (Connection conn = DriverManager.getConnection(LibraryService.MYSQL_URL)){
+		Connection conn = JdbcBookDao.createConnection();
 
-			PreparedStatement statement = null;
-			ResultSet rs = null;
+		PreparedStatement statement = null;
+		ResultSet rs = null;
 
 			try {
-				statement = conn.prepareStatement("SELECT * from acsm_b775c39c99325aa.Book");
+				statement = conn.prepareStatement(GET_ALL_BOOKS);
 				rs = statement.executeQuery();
 				while (rs.next()) {
-					books.add(new Book(rs.getInt("id_book"),rs.getString("title"),rs.getString("author"),rs.getString("edition"),rs.getInt("year")));
+					books.add(mapRow(rs));
 				}
 			} catch (SQLException se) {
 				System.out.println("SQL Error: " + se);
 			}
-		}catch (SQLException se) {
-			System.out.println("Connection failed: " + se);
-		}
+
+		JdbcBookDao.closeConnection(conn);
 
 		return books;
 	}
 
-	private static final RowMapper<Book> rowMapper = new RowMapper<Book>() {
+	private Book mapRow(ResultSet rs) throws SQLException {
+		if(rs == null) return null;
+		Book book = new Book();
 
-		public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return new Book();
-		}
-	};
+		book.setId(rs.getInt("id_book"));
+		book.setAuthor(rs.getString("author"));
+		book.setTitle(rs.getString("title"));
+		book.setEdition(rs.getString("edition"));
+		book.setYear(rs.getInt("year"));
+
+		return book;
+	}
 
 }
